fa15bd036911bfacb1e0ae9a8d4ca966c6676f07
LarisaChAng <LChS81@mail.ru>
2022-12-29 23:55:42 +0800

    Добавьте файлы проекта.

---
+++ MVVM_les19_WpfApp1/Models/Length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_les19_WpfApp1.Models
+{
+    static class Length
+    {        
+        public static double Add(double r) => 2*r*Math.PI;
+    }
+}
---
+++ MVVM_les19_WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using MVVM_les19_WpfApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_les19_WpfApp1.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }        
+
+        private double numberC;
+        public double NumberC
+        {
+            get => numberC;
+            set
+            {
+                numberC = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {            
+            NumberC = Length.Add(Number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Number1 != 0)
+                return true;
+            else 
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
